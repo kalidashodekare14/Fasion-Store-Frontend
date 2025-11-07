9249ae0c5d959fc5d6b89b756dc389390610aa14
kalidashodekare14 <kalidashodekare0@gmail.com>
2025-11-07 08:23:48 +0600

    Project set up completed

---
+++ app/shop/page.tsx
@@ -0,0 +1,11 @@
+import ShopComponent from '@/components/pages/ShopComponent';
+
+const ShopPage = () => {
+    return (
+        <div className='lg:mx-32 mx-5'>
+            <ShopComponent />
+        </div>
+    );
+};
+
+export default ShopPage;
---
+++ components/pages/ShopComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopComponent = () => {
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default ShopComponent;
